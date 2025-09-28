80e10a920a5c47ff11e25b43acccdb3cf1be83d3
Shaun Burdick <github@shaunburdick.com>
2025-09-28 16:19:00 -0400

    feat: upgrade to version 4.0.0 and add new ESLint plugins

--- eslint/react/index.js
+++ eslint/react/index.js
@@ -1,4 +1,6 @@
 import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect';
 import globals from 'globals';
 import rules from './rules.js';
 
@@ -7,6 +9,8 @@ export default [
         files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
         ...reactPlugin.configs.flat.recommended,
         ...reactPlugin.configs.flat['jsx-runtime'],
+        ...reactHooks.configs['recommended-latest'],
+        ...reactYouMightNotNeedAnEffect.configs.recommended,
         languageOptions: {
             ...reactPlugin.configs.flat.recommended.languageOptions,
             globals: {
